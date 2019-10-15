1ed753af5297db414fa126aabb5dcf3d4315f3ea
seanyboy271 <sean.kotrola@gmail.com>
2019-10-15 17:37:04 -0400

    Added web

---
+++ web/src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Yay we did it!
+    </div>
+  );
+}
+
+export default App;
