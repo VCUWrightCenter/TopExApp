f4b86e5f46832f552ca20e1c2e5ec5ed6ceca382
Evan French <etfrench@vcu.edu>
2020-06-20 14:07:43 -0400

    Updating run configurations. Runs locally

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import request, Flask
+from flask import request, Flask, jsonify, make_response
 import sys
 import service
 
@@ -13,6 +13,11 @@ def process():
     try:
         result = service.process(request)
     except:
-        return ("Unexpected error: ", sys.exc_info()[0])
+        response = make_response(jsonify("Unexpected error: ", sys.exc_info()[0]))
     else:
-        return result
+        response = make_response(result)
+
+    # Add Access-Control-Allow-Origin header to allow cross-site request
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+
+    return response
