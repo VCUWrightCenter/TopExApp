8fa28a3dca9ffa3b5cd507070723dded59d07623
Peter Burdette <burdettep@vcu.edu>
2020-09-24 14:42:27 -0400

    Installed material-ui

--- web/src/Components/DetailPanel/DetailPanel.js
+++ web/src/Components/DetailPanel/DetailPanel.js
@@ -2,6 +2,14 @@
 
 import React, { Component } from "react";
 import './DetailPanel.css'
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 
 class DetailPanel extends Component {
 
@@ -57,6 +65,31 @@ class DetailPanel extends Component {
         return (
             <div className='right-sidebar'>
                 {this.state.pointDisplay}
+                <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to
+            Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
             </div>
         );
     }
