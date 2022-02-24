230994edf1cfda8c9aebfc00c71dce5ccda7d782
Evan French <etfrench@vcu.edu>
2022-02-24 12:06:17 -0500

    stable on AWS -minus NER

--- api/install.sh
+++ api/install.sh
@@ -1,4 +1,3 @@
 pip freeze | xargs pip uninstall -y
-pip install Flask Flask-Cors gensim gunicorn matplotlib plotly pymed scipy sklearn spacy textblob umap-learn
+pip install Flask Flask-Cors gensim gunicorn matplotlib numpy==1.21.0 openpyxl pandas plotly pymed scipy sklearn spacy textblob umap-learn
 pip freeze > requirements.txt
-python -m spacy download en_core_web_sm
